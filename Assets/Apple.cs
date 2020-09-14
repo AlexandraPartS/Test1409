067d71768a44de62cb36e092d6c5421523722a1f
Part_S <a.pinyagina@ntc-vulkan.ru>
2020-09-14 12:53:59 +0300

    Go all files to Develop branch

---
+++ Assets/Apple.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    public Color color = Color.green;
+    private GameObject appleGO;
+
+    // Default constructor.
+    public Apple()
+    {
+    }
+
+    public Apple(Color color)
+    {
+       this.color = color;
+    }
+
+    public Apple(GameObject applePrefab, Material material, int x, int y, int z, Color color)
+    {
+        appleGO = Instantiate(applePrefab, new Vector3(x, y, z), Quaternion.AngleAxis(30, Vector3.forward));
+        appleGO.GetComponent<Renderer>().material = material;
+        appleGO.GetComponent<Renderer>().material.color = color;
+
+    }
+}
---
+++ Assets/AppleCreate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleCreate : MonoBehaviour
+{
+    public GameObject applePrefab;
+    public Material material;
+    //private Vector3 pos;
+    //private Color color;
+
+
+
+    void Start()
+    {
+        Apple apple1 = new Apple();
+        Debug.Log(apple1.color);
+
+        Apple apple2 = new Apple(Color.yellow);
+        Debug.Log(apple2.color);
+
+        Apple apple3 = new Apple(applePrefab, material, 1, 2, 0, Color.yellow);
+
+    }
+
+}
